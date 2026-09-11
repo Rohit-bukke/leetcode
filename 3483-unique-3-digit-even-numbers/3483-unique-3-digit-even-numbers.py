e0debdcf64cc7745925c7ca9d56ffa554b6062be
Rohit <r220468@rguktrkv.ac.in>
2026-09-11 05:57:23 +0530

    Time: 15 ms (69.45%), Space: 19.3 MB (72.53%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
@@ -1,33 +1,21 @@
 class Solution:
     def totalNumbers(self, digits: List[int]) -> int:
-        
-        # Count frequency of each digit 0-9
-        freq = [0] * 10
-        for d in digits:
-            freq[d] += 1
-            
-        valid_count = 0
-        
-        # Iterate through all 3-digit even numbers
-        for num in range(100, 1000, 2):
-            h = num // 100
-            t = (num // 10) % 10
-            u = num % 10
-            
-            # Count requirement for current number
-            req = [0] * 10
-            req[h] += 1
-            req[t] += 1
-            req[u] += 1
-            
-            # Check if we have enough of each digit
-            possible = True
-            for i in range(10):
-                if req[i] > freq[i]:
-                    possible = False
-                    break
-                    
-            if possible:
-                valid_count += 1
-                
-        return valid_count
+        n = len(digits)
+        vis = [False] * 1000
+        ans = 0
+
+        for i in range(n):
+            if digits[i] == 0:
+                continue
+            for j in range(n):
+                if j == i:
+                    continue
+                for k in range(n):
+                    if k == i or k == j or digits[k] % 2 != 0:
+                        continue
+                    x = digits[i] * 100 + digits[j] * 10 + digits[k]
+                    if not vis[x]:
+                        vis[x] = True
+                        ans += 1
+
+        return ans
